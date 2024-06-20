40a1b1e743828de674985d8c89718da0123a4b7b
viswa3024 <95619173+viswa3024@users.noreply.github.com>
2024-06-20 14:40:29 +0530

    Update YearSelect.js

--- YearSelect.js
+++ YearSelect.js
@@ -26,3 +26,47 @@ const YearSelect = ({ startYear, endYear, onYearChange }) => {
 };
 
 export default YearSelect;
+
+
+
+
+
+
+
+import React, { useState, useEffect } from 'react';
+
+const YearSelect = ({ onYearChange }) => {
+  const [selectedYear, setSelectedYear] = useState('');
+  const [startYear, setStartYear] = useState(0);
+  const [endYear, setEndYear] = useState(0);
+
+  useEffect(() => {
+    const currentYear = new Date().getFullYear();
+    setEndYear(currentYear);
+    setStartYear(currentYear - 15);
+  }, []);
+
+  const generateYearOptions = () => {
+    const options = [];
+    for (let year = startYear; year <= endYear; year++) {
+      options.push(<option key={year} value={year}>{year}</option>);
+    }
+    return options;
+  };
+
+  const handleChange = (e) => {
+    const year = e.target.value;
+    setSelectedYear(year);
+    onYearChange(year);
+  };
+
+  return (
+    <select value={selectedYear} onChange={handleChange}>
+      <option value="">Select a year</option>
+      {generateYearOptions()}
+    </select>
+  );
+};
+
+export default YearSelect
+
